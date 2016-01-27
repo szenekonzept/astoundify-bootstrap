367111e1a36d72e99ceea6ae9e6d3e2949f2d31d
Spencer Finnell <spencerfinnell@gmail.com>
2016-01-27 13:43:27 -0500

    tweaks and add xml

--- listify.sh
+++ listify.sh
@@ -1,60 +1,90 @@
 #!/bin/bash
 
-ls | grep -v 'listify.sh' | xargs rm -rf
+##
+# Theme
+##
+rm -rf htdocs/wp-content/themes/*
+
+# give one default theme
+vassh wp theme install twentytwelve
+
+# install and activate listify
+cd htdocs/wp-content/themes
+
+git clone git@github.com:astoundify/listify.git
+vassh wp theme activate listify
+
+# back to root
+cd ../../../
+
+##
+# Plugins
+##
+
+rm -rf htdocs/wp-content/plugins/*
 
 # some .org stuff
-wp plugin install debug-bar --activate
-wp plugin install debug-queries --activate
-wp plugin install query-monitor --activate
+vassh wp plugin install debug-bar --activate
+vassh wp plugin install debug-queries --activate
+vassh wp plugin install query-monitor --activate
 
-wp plugin install nav-menu-roles --activate
-wp plugin install rtl-tester --activate
-wp plugin install user-role-editor --activate
-wp plugin install user-switching --activate
+vassh wp plugin install nav-menu-roles --activate
+vassh wp plugin install rtl-tester --activate
+vassh wp plugin install user-role-editor --activate
+vassh wp plugin install user-switching --activate
 
 # woocommerce
-wp plugin install woocommerce --activate
+vassh wp plugin install woocommerce --activate
 
 git clone git@github.com:woothemes/woocommerce-bookings.git
-vsh plugin activate woocommerce-bookings
+vassh wp plugin activate woocommerce-bookings
 
 git clone git@github.com:woothemes/woocommerce-subscriptions.git
-vsh plugin activate woocommerce-subscriptions
+vassh wp plugin activate woocommerce-subscriptions
 
 git clone git@github.com:woothemes/woocommerce-social-login.git
-vsh plugin activate woocommerce-social-login
+vassh wp plugin activate woocommerce-social-login
 
 # wpjobmanager
-wp plugin install wp-job-manager --activate
+vassh wp plugin install wp-job-manager --activate
 
 git clone git@github.com:Automattic/wp-job-manager-bookmarks.git
-vsh plugin activate wp-job-manager-bookmarks
+vassh wp plugin activate wp-job-manager-bookmarks
 
 git clone git@github.com:Automattic/wp-job-manager-alerts.git
-vsh plugin activate wp-job-manager-alerts
+vassh wp plugin activate wp-job-manager-alerts
 
 git clone git@github.com:Automattic/wp-job-manager-tags.git
-vsh plugin activate wp-job-manager-tags
+vassh wp plugin activate wp-job-manager-tags
 
 git clone git@github.com:Automattic/wp-job-manager-wc-paid-listings.git
-vsh plugin activate wp-job-manager-wc-paid-listings
+vassh wp plugin activate wp-job-manager-wc-paid-listings
 
 # astoundify wpjobmanager
 
 git clone git@github.com:astoundify/wp-job-manager-claim-listing.git
-vsh plugin activate wp-job-manager-claim-listing
+vassh wp plugin activate wp-job-manager-claim-listing
 
 git clone git@github.com:astoundify/wp-job-manager-contact-listing.git
-vsh plugin activate wp-job-manager-contact-listing
+vassh wp plugin activate wp-job-manager-contact-listing
 
 git clone git@github.com:astoundify/wp-job-manager-extended-location.git
-vsh plugin activate wp-job-manager-extended-location
+vassh wp plugin activate wp-job-manager-extended-location
 
 git clone git@github.com:astoundify/wp-job-manager-regions.git
-vsh plugin activate wp-job-manager-claim-regions
+vassh wp plugin activate wp-job-manager-regions
 
 git clone git@github.com:astoundify/wp-job-manager-reviews.git
-vsh plugin activate wp-job-manager-claim-reviews
+vassh wp plugin activate wp-job-manager-reviews
 
 git clone git@github.com:astoundify/wp-job-manager-products.git
-vsh plugin activate wp-job-manager-claim-products
+vassh wp plugin activate wp-job-manager-products
+
+# return to root
+cd ../../../
+
+##
+# Content
+##
+# wget https://github.com/spencerfinnell/astoundify-bootstrap/blog/master/listify.xml
+vassh wp import listify.xml
